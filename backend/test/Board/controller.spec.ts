b6e81f84b56a747c9b93318f995658ec4280d1a5
Iliyan-Y <maximizebg@gmail.com>
2023-12-03 10:46:52 +0000

    refactoring test setup

--- backend/test/Board/controller.spec.ts
+++ backend/test/Board/controller.spec.ts
@@ -1,29 +1,13 @@
-import { BoardController } from '../../src/API/Board/controller';
 import { Board } from '../../src/Gateways/Board/entity';
 import * as httpMocks from 'node-mocks-http';
-import { TestingModule } from '@nestjs/testing/testing-module';
-import { Test } from '@nestjs/testing';
-import {
-  BoardService,
-  CreateCommand,
-  CreateResultStatus,
-} from '../../src/Domain/Board/service';
-import { AutomapperModule } from '@automapper/nestjs';
-import { classes } from '@automapper/classes';
-import { BoardProfile } from 'src/Modules/Board/mapper.profile';
-import { BoardGateway } from 'src/Gateways/Board/gateway';
-import { BoardRepository } from 'src/Gateways.DB/Board/repository';
-import { TypeOrmTestingModule } from '../helpers/db';
-import { INestApplication } from '@nestjs/common';
+import { CreateCommand } from '../../src/Domain/Board/service';
 import * as request from 'supertest';
 import { CreateResponse } from 'src/API/Board/Models/createResponse';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { createDefaultTestingModule } from './setup';
 
 describe('board controller', () => {
-  let app: INestApplication;
-  let controller: BoardController;
-
   const mockRequest = httpMocks.createRequest();
   mockRequest.body = {
     boardName: 'Test-123',
@@ -42,34 +26,34 @@ describe('board controller', () => {
   // };
 
   // create fake module
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        AutomapperModule.forRoot({
-          strategyInitializer: classes(),
-        }),
-        ...TypeOrmTestingModule([Board]),
-      ],
-      controllers: [BoardController],
-      providers: [
-        BoardProfile,
-        BoardService,
-        { provide: BoardGateway, useClass: BoardRepository },
-        // { provide: UserService, useValue: mockUserService },
-      ],
-    })
-      // .overrideProvider(BoardService)
-      // .useValue(mockService)
-      .compile()
-      .catch((err) => {
-        console.error(err);
-        throw err;
-      });
+  // beforeEach(async () => {
+  //   const moduleFixture: TestingModule = await Test.createTestingModule({
+  //     imports: [
+  //       AutomapperModule.forRoot({
+  //         strategyInitializer: classes(),
+  //       }),
+  //       ...TypeOrmTestingModule([Board]),
+  //     ],
+  //     controllers: [BoardController],
+  //     providers: [
+  //       BoardProfile,
+  //       BoardService,
+  //       { provide: BoardGateway, useClass: BoardRepository },
+  //       // { provide: UserService, useValue: mockUserService },
+  //     ],
+  //   })
+  //     // .overrideProvider(BoardService)
+  //     // .useValue(mockService)
+  //     .compile()
+  //     .catch((err) => {
+  //       console.error(err);
+  //       throw err;
+  //     });
 
-    controller = moduleFixture.get<BoardController>(BoardController);
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  //   controller = moduleFixture.get<BoardController>(BoardController);
+  //   app = moduleFixture.createNestApplication();
+  //   await app.init();
+  // });
 
   afterEach(() => {
     jest.resetAllMocks();
@@ -80,17 +64,23 @@ describe('board controller', () => {
   //}
 
   it('Example controller test', async () => {
+    const module = await createDefaultTestingModule();
     const command = new CreateCommand('From_the_test');
-    const expected = await controller.create(command);
+    const expected = await module.controller.create(command);
     expect(expected).toEqual(new CreateResponse('From_the_test'));
   });
 
   // Prefer to run  e2e as it will also test the body mapping profile
-  it('When no body then status code 500', () => {
+  it('When no body then status code 500', async () => {
+    const { app } = await createDefaultTestingModule();
+    await app.init();
     return request(app.getHttpServer()).post('/').expect(500);
   });
 
   it('When record with the same name return conflict', async () => {
+    const { app } = await createDefaultTestingModule();
+    await app.init();
+
     const repo = app.get<Repository<Board>>(getRepositoryToken(Board));
     const board = repo.create({ name: 'test' });
     await repo.save(board);
@@ -102,7 +92,10 @@ describe('board controller', () => {
       .expect(409);
   });
 
-  it('When record created, return board object with name', () => {
+  it('When record created, return board object with name', async () => {
+    const { app } = await createDefaultTestingModule();
+    await app.init();
+
     return request(app.getHttpServer())
       .post('/')
       .send({
