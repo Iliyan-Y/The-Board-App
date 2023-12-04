d12fbae4f4aa499af7cea436d4f86ee83ea7d0e7
Iliyan-Y <maximizebg@gmail.com>
2023-12-04 17:47:12 +0000

    Quick theme switch

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" data-theme="dark">
+		<html lang="en" data-theme="black">
 			<body className={inter.className}>
 				{/* conditional render auth ?  */}
 				{/* {false ? <div> Auth component</div> :children} */}
