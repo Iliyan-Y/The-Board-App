0499da4f7d3a81759a4e1b903764be3deb34e3ce
Iliyan-Y <maximizebg@gmail.com>
2023-12-16 17:09:55 +0000

    Wip update command

--- backend/NestJs/src/API/Task/controller.ts
+++ backend/NestJs/src/API/Task/controller.ts
@@ -8,6 +8,7 @@ import {
   HttpStatus,
   Param,
   Post,
+  Put,
 } from "@nestjs/common";
 import { CreateRequest } from "./Models/CreateRequest";
 import {
@@ -24,6 +25,12 @@ import {
   ListService,
 } from "src/Domain/Task/services/list";
 import { ListByColumnIdResult } from "./Models/ListByColumnIdResult";
+import { UpdateRequest } from "./Models/UpdateModel";
+import {
+  UpdateCommand,
+  UpdateResultStatus,
+  UpdateTaskService,
+} from "src/Domain/Task/services/update";
 
 @Controller("task")
 export class TaskController {
@@ -31,6 +38,7 @@ export class TaskController {
     @InjectMapper() private readonly mapper: Mapper,
     private readonly createService: CreateTaskService,
     private readonly listService: ListService,
+    private readonly updateService: UpdateTaskService,
   ) {}
 
   @Post()
@@ -56,7 +64,7 @@ export class TaskController {
   }
 
   @Get(":columnId")
-  async get(
+  async getByColumnID(
     @Param(MapPipe(ListByColumnIdRequest, ListByColIdCommand))
     command: ListByColIdCommand,
   ) {
@@ -70,6 +78,32 @@ export class TaskController {
         );
       case ListResultStatus.NotFound:
         return [];
+      default:
+        throw Error(
+          `An unexpected issue occurred when creating Board. Result Status was: ${result.status}`,
+        );
+    }
+  }
+
+  @Put()
+  async updateTask(
+    @Body() body: UpdateRequest,
+    @Body(MapPipe(UpdateRequest, UpdateCommand)) command: UpdateCommand,
+  ) {
+    console.log("Updating task for ", command.id);
+    const result = await this.updateService.update(command);
+
+    switch (result.status) {
+      case UpdateResultStatus.Updated:
+        return result.model;
+      // return result.model.map((task) =>
+      //   this.mapper.map(task, TaskModel, ListByColumnIdResult),
+      // );
+      case UpdateResultStatus.FailedToUpdate:
+        throw new HttpException(
+          `Task with name ${command.name} failed to update`,
+          HttpStatus.BAD_REQUEST,
+        );
       default:
         throw Error(
           `An unexpected issue occurred when creating Board. Result Status was: ${result.status}`,
--- backend/NestJs/src/Domain/Task/services/update.ts
+++ backend/NestJs/src/Domain/Task/services/update.ts
@@ -1,4 +1,10 @@
 import { AutoMap } from "@automapper/classes";
+import { TaskModel } from "../mode";
+import { Injectable } from "@nestjs/common";
+import { TaskGateway } from "src/Gateways/Task/gateway";
+import { InjectMapper } from "@automapper/nestjs";
+import { Mapper } from "@automapper/core";
+import { Task } from "src/Gateways/Task/entity";
 
 export class UpdateCommand {
   @AutoMap()
@@ -18,5 +24,40 @@ export class UpdateCommand {
     this.name = name;
     this.columnId = columnId;
     this.description = description;
+  }
+}
+
+export enum UpdateResultStatus {
+  Updated = 1,
+  FailedToUpdate = "FailedToUpdate",
+}
+
+class UpdateResult {
+  status: UpdateResultStatus;
+  model: TaskModel;
+
+  constructor(status: UpdateResultStatus, model?: TaskModel) {
+    (this.model = model), (this.status = status);
+  }
+}
+
+abstract class UpdateTask {
+  abstract update(command: UpdateCommand): Promise<UpdateResult>;
+}
+@Injectable()
+export class UpdateTaskService implements UpdateTask {
+  constructor(
+    private readonly gateway: TaskGateway,
+    @InjectMapper() private readonly mapper: Mapper,
+  ) {}
+
+  async update(command: UpdateCommand): Promise<UpdateResult> {
+    const model = this.mapper.map(command, UpdateCommand, Task);
+    const task = await this.gateway.update(model);
+    const taskModel = this.mapper.map(task, Task, TaskModel);
+
+    if (!taskModel) return new UpdateResult(UpdateResultStatus.FailedToUpdate);
+
+    return new UpdateResult(UpdateResultStatus.Updated, taskModel);
   }
 }
